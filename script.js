b913bfd3dc62e5f7f2861684a8c80d1c40f65fb9
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-04 05:44:56 -0500

    Added the if statement to clear the timer interval, so it would not go on and on forever

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var submitButton = document.getElementById("submitButton");
 var timer = document.getElementById("timer"); // Timer Variable 
 
 function startQuiz() { 
-    secondsLeft = 91;
+    secondsLeft = 5;
     
       var timerInterval = setInterval(function() { 
         secondsLeft--;
         timer.textContent = "Time: " + secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+          }
         }, 1000);
     }
 
