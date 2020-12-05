f904b333e60ba820387c98aec071f2fe21308d4d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-05 13:40:59 -0500

    Added quiz questions array and populated with a question. This required variables for each choice. Created if statement for the timer to look for the index and add the questions

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ var quizChallengePage = document.getElementById("quizChallengePage"); // Initial
 var quizQuestionsPage = document.getElementById("quizQuestionsPage"); // Quiz page variable
 var quizQuestionHeader = document.getElementById("quizQuestionHeader");
 
+var choice1 = document.getElementById("one");
+var choice2 = document.getElementById("two");
+var choice3 = document.getElementById("three");
+var choice4 = document.getElementById("four");
+
+// Quiz Questions array
+var quizQuestions = [
+    {
+    "quizQuestionHeader" : "Which is not a JavaScript Data type:", 
+    "one" : "1. number",
+    "two" : "2. string",
+    "three" : "3. variable",
+    "four" : "4. boolean",
+    "correct" : "2. string",
+    },
+]
+
+var questionIndex = 0;
 
 function homePage() {
     quizChallengePage.style.display = "block"; // Hide first page after Start Quiz button is clicked
@@ -20,17 +38,47 @@ function startQuiz() {
     quizQuestionsPage.style.display = "block"; // Next, show quiz questions page 
     console.log("page1 loaded")
 
-    secondsLeft = 5; //Testing with 5 sec to see what happens
+    secondsLeft = 50; //Testing with 50 sec to see what happens
         
     //Countdown that ends the quiz
       var timerInterval = setInterval(function() { 
         secondsLeft--;
         timer.textContent = "Time: " + secondsLeft;
-        if (secondsLeft === 0) {
+        if (secondsLeft === 0 || quizQuestions.length === questionIndex) {
             clearInterval(timerInterval);
           }
         }, 1000);
     }
+
+    function showQuestions() {
+        var q = quizQuestions[questionIndex];
+      
+        quizQuestionHeader.innerHTML = q.quizQuestionHeader;
+        choice1.innerHTML = q.one;
+        choice1.setAttribute("data-answer", q.one);
+        choice2.innerHTML = q.two;
+        choice2.setAttribute("data-answer", q.two);
+        choice3.innerHTML = q.three;
+        choice3.setAttribute("data-answer", q.three);
+        choice4.innerHTML = q.four;
+        choice4.setAttribute("data-answer", q.four);
+      }
+
+    // Event Listeners
+    showQuestions();
+
+    choice1.addEventListener("click", function (event) {
+        checkAnswer(event);
+    })
+    choice2.addEventListener("click", function (event) {
+        checkAnswer(event);
+    })
+    choice3.addEventListener("click", function (event) {
+        checkAnswer(event);
+    })
+    choice4.addEventListener("click", function (event) {
+        checkAnswer(event);
+    })
 
     // Start quiz clock when the button is pushed
     submitButton.addEventListener("click", function() { 
