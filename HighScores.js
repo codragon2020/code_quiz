f57b39e9d769038af53775795a888e35c0026c6c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-12 14:52:26 -0500

    Cleaning up console.logs in HighScores.js

--- HighScores.js
+++ HighScores.js
@@ -9,7 +9,7 @@ clearHighScore.addEventListener("click", function () {
     location.reload();
 });
 
-// Retreives local stroage 
+// Retreives localStorage 
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
@@ -27,26 +27,19 @@ function sortList() {
     var list, i, switching, b, shouldSwitch;
     list = document.getElementById("createLi");
     switching = true;
-    console.log('list is sorting');
 
     /* Make a loop that will continue until
     no switching has been done: */
     while (switching) {
-        console.log('list is sorting');
-
-        // Start by saying: no switching is done:
+      // Start by saying: no switching is done:
       switching = false;
-      console.log('switching', switching);
       b = document.getElementsByTagName("LI");
-      console.log('this is b', b);
       // Loop through all list items:
       for (i = 0; i < (b.length - 1); i++) {
         // Start by saying there should be no switching:
         shouldSwitch = false;
-        // console.log('shouldSwitch', shouldSwitch);
         /* Check if the next item should
         switch place with the current item: */
-        console.log('list is sorting');
 
         if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
           /* If next item is alphabetically lower than current item,
@@ -60,7 +53,6 @@ function sortList() {
         and mark the switch as done: */
         b[i].parentNode.insertBefore(b[i + 1], b[i]);
         switching = true;
-        console.log('list is sorting');
       }
     }
   }
